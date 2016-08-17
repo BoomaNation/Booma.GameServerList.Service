7d87116ba5330758f5f87ae6397da97904124226
Andrew Blakely <andrew.blakely@ymail.com>
2016-08-17 00:46:26 -0500

    Added Details Repo

---
+++ src/Booma.GameServerList.Lib/Repositories/GameServerDetailsRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Booma.GameServerList.Lib
+{
+	public class GameServerDetailsRepository : IGameServerDetailsRepositoryAsync
+	{
+		/// <summary>
+		/// Database context.
+		/// </summary>
+		private GameServerListDbContext gameserverListContext { get; }
+
+		/// <summary>
+		/// Creates a new gameserver repo.
+		/// </summary>
+		/// <param name="context">Database context.</param>
+		public GameServerDetailsRepository(GameServerListDbContext context)
+		{
+			gameserverListContext = context;
+		}
+
+		public async Task<IEnumerable<GameServerDetailsModel>> GetAllPublic()
+		{
+			return await gameserverListContext.GameServers
+				.ToAsyncEnumerable()
+				.Where(gs => gs.Status.HasFlag(ServerStatus.Public | ServerStatus.Online)) //select online public servers
+				.ToList();
+		}
+	}
+}
