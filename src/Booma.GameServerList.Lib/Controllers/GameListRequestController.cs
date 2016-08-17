f3a28597a8f332cebba87929009c54cb5a7ba270
Andrew Blakely <andrew.blakely@ymail.com>
2016-08-17 00:57:16 -0500

    Fleshed out GameserverList controller

---
+++ src/Booma.GameServerList.Lib/Controllers/GameListRequestController.cs
@@ -0,0 +1,48 @@
+﻿using Booma.Payloads.ServerSelection;
+using GladNet.ASP.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GladNet.Payload;
+
+namespace Booma.GameServerList.Lib
+{
+	/// <summary>
+	/// Controller that handles GladNet <see cref="GameServerListRequestPayload"/>s.
+	/// </summary>
+	public class GameListRequestController : RequestController<GameServerListRequestPayload>
+	{
+		/// <summary>
+		/// Gameserver details repo.
+		/// </summary>
+		private IGameServerDetailsRepositoryAsync gameserverDetailsRepoService { get;}
+
+		public GameListRequestController(IGameServerDetailsRepositoryAsync repo)
+		{
+			gameserverDetailsRepoService = repo;
+		}
+
+		/// <summary>
+		/// Called by GladNet when a <see cref="GameServerListRequestPayload"/> is recieved by the
+		/// ASP controller.
+		/// </summary>
+		/// <param name="payloadInstance"></param>
+		/// <returns>A <see cref="GameServerListResponsePayload"/>.</returns>
+		public override async Task<PacketPayload> HandlePost(GameServerListRequestPayload payloadInstance)
+		{
+			GameServerListResponseCode responseCode = GameServerListResponseCode.Unknown;
+
+			IEnumerable<GameServerDetailsModel> details = await gameserverDetailsRepoService.GetAllPublic();
+
+			//Check if we have any servers
+			if (details.Count() == 0)
+				responseCode = GameServerListResponseCode.ServiceUnavailable;
+			else
+				responseCode = GameServerListResponseCode.Success;
+
+			//Build a response payload and map the DB model to the wire-type model.
+			return new GameServerListResponsePayload(responseCode, details.Select(d => new SimpleGameServerDetailsModel(d.Name, d.Address, d.ServerPort, d.Region)));
+		}
+	}
+}
---
+++ src/Booma.GameServerList.Lib/Enums/ServerStatus.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Booma.GameServerList.Lib
+{
+	/// <summary>
+	/// Flags indicating the server type.
+	/// </summary>
+	public enum ServerStatus : int
+	{
+		Offline = 1 << 0,
+		Online = 1 << 1,
+		Public = 1 << 2,
+		Test = 1 << 3,
+		Private = 1 << 4
+	}
+}
