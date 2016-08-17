e33248f7d1e77063e63deb475c36a78644213bb3
Andrew Blakely <andrew.blakely@ymail.com>
2016-08-17 00:46:35 -0500

    Added DBContext

---
+++ src/Booma.GameServerList.Lib/Database/GameServerListDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+
+namespace Booma.GameServerList.Lib
+{
+	/// <summary>
+	/// Context for the gameserver list.
+	/// </summary>
+	public class GameServerListDbContext : DbContext
+	{
+		/// <summary>
+		/// The available gameservers.
+		/// </summary>
+		public DbSet<GameServerDetailsModel> GameServers { get; private set; }
+
+		public GameServerListDbContext(DbContextOptions options) 
+			: base(options)
+		{
+
+		}
+	}
+}
