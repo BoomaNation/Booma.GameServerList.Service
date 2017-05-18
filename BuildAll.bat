d473eaf97e8cdc64c6e9a58b99a90fa14b581e97
HelloKitty <andrew.blakely@ymail.com>
2017-05-18 15:14:58 -0500

    Update config and build files

--- BuildAll.bat
+++ BuildAll.bat
@@ -3,14 +3,14 @@ dotnet restore
 dotnet build src/GladLive.ModuleService.ASP -c Release
 cd ..
 
-cd src/Booma.GameServerList.Module
+cd src/Booma.GameServerList.Service
 dotnet restore
 dotnet build -c Release
 cd ..
 cd ..
 
 xcopy GladLive.ModuleService.ASP\src\GladLive.ModuleService.ASP\bin\Release\net451 build /s /y
-xcopy src\Booma.GameServerList.Module\bin\Release\net451 build\Modules\GameServerList\ /s /y
+xcopy src\Booma.GameServerList.Service\bin\Release build\Modules\GameServerList\ /s /y
 xcopy config build /s /y
 
 PAUSE
